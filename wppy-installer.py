819fd55fbe7e16adb33ef06b8f22daddc19bdbac
Halil Kaya <kayahalil@gmail.com>
2014-09-05 20:50:55 +0300

    wppy-installer.py updated

--- wppy-installer.py
+++ wppy-installer.py
@@ -6,3 +6,10 @@ import os
 os.system('cp wppy.py /usr/bin/')
 os.system('mv /usr/bin/wppy.py /usr/bin/wppy')
 os.system('chmod a+x /usr/bin/wppy')
+
+print
+
+print "wppy installed successfully!"
+print "Just run wppy --name blogname in the folder that you want to install your WordPress blog!"
+
+print
